5fd34118ac4de9ef0df7d61858ef3f13efbc9cbf
Joaquincamps <joaquin.campsmartinez@educa.madrid.org>
2026-02-25 19:11:46 +0100

    Primer commit

---
+++ src/test/java/com/example/GestionReservasHotel/GestionReservasHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GestionReservasHotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionReservasHotelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
